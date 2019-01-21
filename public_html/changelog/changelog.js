3b07768e17de327db67cf6340072992afefc60d1
ruzsaz <ruzsaz@ruzsaz-aeek>
2019-01-21 12:33:58 +0100

    Első commit

--- public_html/changelog/changelog.js
+++ public_html/changelog/changelog.js
@@ -16,8 +16,9 @@ var changelog = [
             {"entity": "AgnosServer", "date": "2018-04-20", "change": "Áttérés Wildfly alkalmazáskonténerre a biztonságosabb autentikáció érdekében."},
             {"entity": "Agnos", "date": "2018-05-02", "change": "Új panel: 2 dimenziós vonaldiagram."},
             {"entity": "Agnos", "date": "2018-05-24", "change": "Egy kiválasztott panel duplájára nagyítható."},
+            {"entity": "Agnos", "date": "2018-11-11", "change": "A help oldalak angol fordítása."},
         ],
-        "updated": "2018-05-24"
+        "updated": "2018-11-11"
     },
     {
         "language": "en",
@@ -26,15 +27,16 @@ var changelog = [
             {"entity": "AgnosCSVImporter", "date": "2017-10-24", "change": "Automatic processing of .xls and .xlsx files."},
             {"entity": "AgnosServer", "date": "2017-12-01", "change": "Localization supported in the server."},
             {"entity": "Agnos", "date": "2017-12-13", "change": "Panels can be rearranged."},
-            {"entity": "Agnos", "date": "2018-01-02", "change": "Division by zero allowed."},
+            {"entity": "Agnos", "date": "2018-01-02", "change": "Division by zero allowed. (Hmmm...)"},
             {"entity": "Agnos, AgnosServer", "date": "2018-01-10", "change": "Refactoring of internationalization of the xml files."},
             {"entity": "AgnosETR", "date": "2018-01-15", "change": "Dynamic extraction of sales managers."},
             {"entity": "Agnos", "date": "2018-01-18", "change": "Minor bugfixes."},
             {"entity": "AgnosServer", "date": "2018-04-20", "change": "Migration to Wildfly application server to increase security."},
-            {"entity": "Agnos", "date": "2018-05-02", "change": "New panel: linechart for 2 dimenzions."},
+            {"entity": "Agnos", "date": "2018-05-02", "change": "New panel: 2-dimensional linechart."},
             {"entity": "Agnos", "date": "2018-05-24", "change": "A selected panel can be magnified to 2x."},
+            {"entity": "Agnos", "date": "2018-11-11", "change": "English translation of the help pages."},
 
         ],
-        "updated": "2018-05-24"
+        "updated": "2018-11-11"
     }
 ];
